3e958cc9b0060841ffbcaebd9fca307f7f70b254
user <user@user.com>
2026-05-07 18:11:57 +0200

    poprawka kalkulatora

--- binary.py
+++ binary.py
@@ -7,5 +7,5 @@ def licz(a):
         return "Podana wartosc nie jest liczba naturalna"
     if a > 100:
         return "Podana wartosc jest poza zakresem"
-    wynik = bin(a)
+    wynik = bin(a)[2:]
     return str(wynik)
