8cf8b7a741165a5d836616551d5000f9528d5e4b
user <user@user.com>
2026-05-07 17:51:25 +0200

    Poprawa pliku utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,21 @@
-# Exemplary calculator functions
+"""Exemplary calculator functions"""
 
 
 def add(a: int, b: int) -> int:
+    """dodawanie"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """odejmowanie"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """mnożenie"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """dzielenie"""
     return a / b
